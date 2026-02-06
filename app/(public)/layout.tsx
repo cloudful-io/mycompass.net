7d773e4663046c1c0f278a790d4caf21a530d631
Will Yip <will@cloudful.io>
2026-02-05 21:04:21 -0500

    Created about and features pages

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,7 +1,6 @@
 import Header from './components/Header'
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
-  console.log("Rendering PublicLayout with children:", children)
   return (
     <div>
       <Header />
